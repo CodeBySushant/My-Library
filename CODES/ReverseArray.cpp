17ac9b7a29418949251fa836ae357f863bb13fcd
Sushant Sharma <itsofficialsushant@gmail.com>
2024-11-24 02:00:26 +0530

    Reversed an array

---
+++ CODES/ReverseArray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+void takeArray(int n, int arr[]){
+    // Taking array as input
+    for(int i=0; i<n; i++){
+        cout<<"Enter value of array "<<i<<" : ";
+        cin>>arr[i];
+    }
+    // Printing taken array elements
+    cout<<"The array elements are: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+};
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    takeArray(n, arr);
+    cout<<endl;
+    int temp;
+    // Bubble Sorting Given Elements
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(arr[j]<arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    // Printing Reversed array
+    cout<<"The reversed sorted elements are: ";
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
