76d7a9e248a553723dc2cfaca5c25d9236926650
Sushant Sharma <itsofficialsushant@gmail.com>
2024-11-24 01:13:32 +0530

    Just adding instructions in my code

--- CODES/BubbleSort.cpp
+++ CODES/BubbleSort.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
 void takeArray(int n, int arr[]){
+    // Taking array as input
     for(int i=0; i<n; i++){
         cout<<"Enter value of array "<<i<<" : ";
         cin>>arr[i];
     }
+    // Printing taken array elements
     cout<<"The array elements are: ";
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
@@ -18,15 +20,18 @@ int main(){
     takeArray(n, arr);
     cout<<endl;
     int temp;
+    // Bubble Sort Program
     for(int i=0; i<n; i++){
         for(int j=0; j<n-i-1; j++){
             if(arr[j]>arr[j+1]){
+                // Swapping values for sorting
                 temp = arr[j];
                 arr[j]= arr[j+1];
                 arr[j+1] = temp;
         }
     }
-}
+}   
+// Printing sorted array
     cout<<"The sorted elements are: ";
     for(int i=0; i<n; i++){
         cout<<arr[i]<<" ";
