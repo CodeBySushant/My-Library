6679aa4f26f9a917458d756adc671dbf15e69504
Sushant Sharma <itsofficialsushant@gmail.com>
2024-11-30 02:53:14 +0530

    Started to complete fibonacci series problem

--- CODES/FibonacciSeries.cpp
+++ CODES/FibonacciSeries.cpp
@@ -1,2 +1,13 @@
 #include<iostream>
 using namespace std;
+void Fibonacci(){
+    int n;
+    int fibo;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    fibo(n-1)+fibo(n-2);
+    cout<<fibo;
+};
+int main(){
+    Fibonacci();
+}
