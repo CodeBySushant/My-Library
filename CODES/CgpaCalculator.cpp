9f818ced341181d6dc3262f64cb51bbee30c4a21
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-01 15:22:08 +0530

    Solving CGPA calculator program but not completed yet.

---
+++ CODES/CgpaCalculator.cpp
@@ -0,0 +1,28 @@
+// To calculate cgpa from sgpa
+#include<iostream>
+using namespace std;
+class sgpa{
+    public:
+    float cgpa;
+    void takeinputsgpa(){
+        cout<<""
+    }
+    void calculateCgpa(int sgpa){
+        cgpa = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8)/8;
+        cout<<"Your CGPA is:"<<" "<<cgpa;
+    }
+};
+int main(){
+    sgpa s1;
+    sgpa s2;
+    sgpa s3;
+    sgpa s4;
+    sgpa s5;
+    sgpa s6;
+    sgpa s7;
+    sgpa s7;
+
+
+
+    cout<<"Enter your semester GPA: ";
+}
