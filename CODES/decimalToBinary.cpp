c53c37346d9354c83fffe6df8823b23bcc4526d5
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-02 23:50:22 +0530

    Improved Decimal to Binary Code

--- CODES/decimalToBinary.cpp
+++ CODES/decimalToBinary.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 int main(){
     int num;
+    int rem;
     cout<<"Enter the number in decimal: ";
     cin>>num;
-    cout<<num;
+    cout<<num<<endl;
+    for(int i=0; i<=num; i++){
+        if(num>=0){
+            rem = num % 2;
+            cout<<rem;
+        }
+    }
+    return 0;
 }
