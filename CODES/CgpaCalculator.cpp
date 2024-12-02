32cbeafd0473d95e4c8fc708be2bf969b3d6dfe1
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-03 00:36:56 +0530

    Fixed some mistake in cgpa calculator question

--- CODES/CgpaCalculator.cpp
+++ CODES/CgpaCalculator.cpp
@@ -1,29 +1,21 @@
 // To calculate cgpa from sgpa
 #include<iostream>
 using namespace std;
-class sgpa{
-    public:
-    float cgpa;
-    void takeinputsgpa(int n){
-        cout<<"Enter your sgpa: ";
-        cin>>n;
-    }
-    void calculateCgpa(int sgpa){
-        cgpa = (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8)/8;
-        cout<<"Your CGPA is:"<<" "<<cgpa;
-    }
-};
 int main(){
-    sgpa s1;
-    sgpa s2;
-    sgpa s3;
-    sgpa s4;
-    sgpa s5;
-    sgpa s6;
-    sgpa s7;
-    sgpa s7;
-
-
-
-    cout<<"Enter your semester GPA: ";
+    int arr[8];
+    int sum = 0;
+    for(int i=0; i<8; i++){
+        if(arr[i]>0 || arr[i]<10){
+            cout<<"Enter you Semester "<<i+1<<" SGPA (0-10): ";
+            cin>>arr[i];
+            sum = sum + arr[i]; //Adds elements of array
+        }
+        else{
+        cout<<"Invalid input"<<endl;
+        }
+    }
+    int cgpa = sum / 8;
+    cout<<"Your cgpa is: ";
+    cout<<cgpa;
+    return 0;
 }
