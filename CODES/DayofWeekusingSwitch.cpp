afa3a5480c02e37c515f7fd6b5bd61a42b0844d2
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-03 00:05:02 +0530

    Completed an incomplete code

--- CODES/DayofWeekusingSwitch.cpp
+++ CODES/DayofWeekusingSwitch.cpp
@@ -1,7 +1,33 @@
 #include<iostream>
 using namespace std;
 int main(){
-    string day;
-    cout<<"Enter the day of week: ";
+    int day;
+    cout<<"Enter a day of week between (0-7): ";
     cin>>day;
+    switch(day){
+        case 1: 
+        cout<<"Sunday";
+        break;
+        case 2:
+        cout<<"Monday";
+        break;
+        case 3:
+        cout<<"Tuesday";
+        break;
+        case 4:
+        cout<<"Wednesday";
+        break;
+        case 5:
+        cout<<"Thursday";
+        break;
+        case 6:
+        cout<<"Friday";
+        break;
+        case 7:
+        cout<<"Saturday";
+        break;
+        default:
+        cout<<"Invalid day!";
+    }
+    return 0;
 }
