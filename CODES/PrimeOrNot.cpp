32ca441dff6dca2e8ebffd8d5914e33af6a4b298
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-04 03:56:57 +0545

    Prime Or Not Checking Problem

---
+++ CODES/PrimeOrNot.cpp
@@ -0,0 +1,20 @@
+// Not solved yet
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n>1){
+        if(n%1==0 || n%n==0){
+            cout<<"The number is a prime number.";
+        }
+        else if(n%i==0){
+            cout<<"The number is not a prime number.";
+        }
+    }
+    else{
+        cout<<"The number is not a prime number.";
+    }
+    return 0;
+}
