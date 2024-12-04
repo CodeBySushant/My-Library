4625ee78d95920f52c455c94e90d68240296bf1d
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-04 17:17:34 +0545

    Completed prime number program

--- CODES/PrimeOrNot.cpp
+++ CODES/PrimeOrNot.cpp
@@ -1,20 +1,51 @@
-// Not solved yet
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter a number: ";
+//     cin>>n;
+//     if(n%1==0){
+//         for(int i=2; i<n-1; i++){
+//             if(n%i==0){
+//                 cout<<"The number is not a prime number.";
+//                 break;
+//             }
+//             else{
+//                 cout<<"The number is a prime number.";
+//                 break;
+//             }
+//         }
+//     }
+//     return 0;
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter a number: ";
+    cout<<"Enter any number: ";
     cin>>n;
-    if(n>1){
-        if(n%1==0 || n%n==0){
-            cout<<"The number is a prime number.";
-        }
-        else if(n%i==0){
-            cout<<"The number is not a prime number.";
-        }
+
+    if(n<=1){
+        cout<<"Not a prime number!";
+    }
+    else if(n==2){
+        cout<<"Is a Prime number!";
     }
     else{
-        cout<<"The number is not a prime number.";
+        bool isPrime = true;
+        for(int i=2; i<n; i++){
+            if(n%i==0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime){
+        cout<<"Is a Prime Number.";
+    }
+    else{
+        cout<<"Not a Prime Number.";
+    }
     }
     return 0;
 }
