c5ff8c03d6071ef10a46538ea153a88193f89ba3
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-06 22:04:38 +0545

    Few Changes

---
+++ CODES/SumofEvenNumbers.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter how many even number you want: ";
+    cin>>n;
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        cout<<2 * i<<" ";
+        sum = 
+    }
+}
