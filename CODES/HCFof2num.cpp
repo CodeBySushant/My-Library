f84b3b3ed79c7790509fe414b756b0adc9cc4033
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-08 22:15:53 +0545

    Written the same program in C++

--- CODES/HCFof2num.cpp
+++ CODES/HCFof2num.cpp
@@ -1,32 +1,19 @@
-#include <stdio.h>
+#include<iostream>
+using namespace std;
 
-/* Function declaration */
-int gcd(int a, int b);
+int gcd(int a, int b){
+        if(b == 0){
+            return a;
+        }
+        else{
+            return gcd(b, a%b);
+        }
+    }
 
-
-int main()
-{
+int main(){
     int num1, num2, hcf;
-    
-    /* Input two numbers from user */
-    printf("Enter any two numbers to find GCD: ");
-    scanf("%d%d", &num1, &num2);
-    
+    cout<<"Enter two numbers whose HCF is required: ";
+    cin>>num1>>num2;
     hcf = gcd(num1, num2);
-    
-    printf("GCD of %d and %d = %d", num1, num2, hcf);
-    
-    return 0;
-}
-
-
-/**
- * Recursive approach of euclidean algorithm to find GCD of two numbers
- */
-int gcd(int a, int b)
-{
-    if(b == 0)
-        return a;
-    else
-        return gcd(b, a%b); 
+    cout<<"The HCF of "<<num1<<" and "<<num2<<" is: "<<hcf;
 }
