071402e2104ca68412cfb7e8687cfb9a940b50b4
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-10 19:35:29 +0545

    Commited a important step

--- CODES/FindLengthofString.cpp
+++ CODES/FindLengthofString.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(){
     string line;
     cout<<"Enter a sentence: ";
-    getline(cin, line);
+    getline(cin, line); //Takes whole line as input
     cout<<line.length(); //Or we can also use word.size() instead of length
 }
