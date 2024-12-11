30674f4bbbfd335e4d80ab721a35160e4906e5a7
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-11 23:11:48 +0545

    FIxed a minor issue in this code

--- CODES/LeapYearorNot.cpp
+++ CODES/LeapYearorNot.cpp
@@ -4,10 +4,11 @@ int main(){
     int year;
     cout<<"Enter a year: ";
     cin>>year;
-    if(year%4==0){
+    if((year%4==0 && year % 100 != 0)|| (year % 400 == 0)){
         cout<<"Its a leap year!";
     }
     else{
         cout<<"Its not a leap year!";
     }
+    return 0;
 }
