3385d3cad1578f8f7e9718a8ca596e6c94e0335a
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-12 22:15:47 +0545

    Created Functions to Input Details, Show Details and Deposit. More to go

--- CODES/Account_Mgmt.cpp
+++ CODES/Account_Mgmt.cpp
@@ -1,14 +1,37 @@
 #include<iostream>
 using namespace std;
 class BankAccount{
-    public:
+    private:
     int AccountNumber;
     string AccountHolderName;
     float Balance;
-    
 
+    public:
+    void InputDetails(){
+        cout<<"Enter Account Number: ";
+        cin>>AccountNumber;
+        cout<<"Enter Account Holder Name: ";
+        cin>>AccountHolderName;
+        cout<<"Enter Balance: ";
+        cin>>Balance;
+        cout<<endl;
+    }
+    void Deposit(){
+        float amount;
+        cout<<"Enter amount to deposit: ";
+        cin>>amount;
+        Balance = Balance + amount;
+    }
+    void ShowDetails(){
+        cout<<"Account Number: "<<AccountNumber<<endl;
+        cout<<"Account Holder Name: "<<AccountHolderName<<endl;
+        cout<<"Account Balance: "<<Balance<<endl;
+    }
 };
+
 int main(){
     BankAccount obj1;
-    obj1.AccountNumber;
+    obj1.InputDetails();
+    obj1.Deposit();
+    obj1.ShowDetails();
 }
