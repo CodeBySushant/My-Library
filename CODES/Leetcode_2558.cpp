29bfb521bca65d1539e9ca029cb73556b89b8eca
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-14 19:02:07 +0545

    Completed Leetcode Problem Number 2558.

---
+++ CODES/Leetcode_2558.cpp
@@ -0,0 +1,39 @@
+// Take Gifts From the Richest Pile
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter size of array: ";
+    cin>>num;
+    int* gifts = new int[num];
+
+    // Take array elements as input
+    cout<<"Enter the values in array: ";
+    for(int i=0; i<num; i++){
+        cin>>gifts[i];
+    }
+
+    for (int i = 0; i < num - 1; i++) {
+        for (int j = 0; j < num - i - 1; j++) {
+            if (gifts[j] < gifts[j + 1]) {
+                // Swap numbers[j] and numbers[j+1]
+                int temp = gifts[j];
+                gifts[j] = gifts[j + 1];
+                gifts[j + 1] = temp;
+            }
+        }
+    }
+
+    // Gives square root of elements and at last sums it
+    int key;
+    cout<<"Enter key value: ";
+    cin>>key;
+    int sum = 0;
+    for (int i = 0; i<key; i++){ 
+        sum += sqrt(gifts[i]);
+    }
+    cout<<endl;
+    cout<<sum;
+return 0;
+}
