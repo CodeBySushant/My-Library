6be4a6d98f774baff60ae118d762da069d32853f
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-14 16:40:33 +0545

    Program to convert Integer to Char

---
+++ CODES/Int_to_char.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int InputNum;
+    cin>>InputNum;
+    char word = (char)InputNum; //Converts Integer value of InputNum into Character value
+    cout<<word;
+}
