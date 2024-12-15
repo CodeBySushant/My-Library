a5aa504a3c28bd876eb634d1bf8b76c4d633ba3e
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-15 19:15:34 +0545

    Started writing Library System using class.

---
+++ CODES/LibraryMngt.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class LibrarySystem{
+    private: 
+    int id;
+    string name;
+    public:
+    string book_name;
+    int price;
+    string author_name;
+
+    void SetInfo(int id, string name){
+        cout<<"Enter Your id number: ";
+        cin>>id;
+        cout<<"Enter you name: ";
+        cin>>name;
+    }
+
+        int main(){
+        LibrarySystem student;
+
+        student.SetInfo();
+        }
+};
