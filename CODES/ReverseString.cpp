e3a2979f8450d1248fee8b0b99805acc0518311d
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-17 20:23:41 +0545

    Completed program to reverse a string

--- CODES/ReverseString.cpp
+++ CODES/ReverseString.cpp
@@ -3,7 +3,14 @@
 
 using namespace std;
 int main(){
-    string word;
-    cin>>word;
-    cout<<word;
+    string str, reversed;
+    cout<<"Enter any word: ";
+    cin>>str;
+
+    for (int i = str.length() - 1; i >= 0; i--) {   //Reverse every single letter from word
+        reversed += str[i];
+    }
+
+    cout<<"The reverse of that word is: ";
+    cout<<reversed;
 }
