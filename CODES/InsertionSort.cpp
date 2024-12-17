a765c85dc62d5bd567ac7951b23f580f342ddf53
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-17 20:26:14 +0545

    Completed Insertion Sort Program

---
+++ CODES/InsertionSort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i]; // Current element to be sorted
+        int j = i - 1;
+
+        // Move elements greater than 'key' one step ahead
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key; // Place 'key' at the correct position
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    insertionSort(arr, n);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    return 0;
+}
