4240489b2075c950d5d5fad43c9081fada33c1fd
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-23 23:30:07 +0545

    Declared Reference Date To Continue Program

--- CODES/Date_ConversionBSAD.cpp
+++ CODES/Date_ConversionBSAD.cpp
@@ -1,6 +1,15 @@
-// Date COnversion from AD to BS and BS to AD
+// Date Conversion from AD to BS and BS to AD
 #include<iostream>
 using namespace std;
 int main(){
-    int 
+    const int REF_BS_YEAR = 2000;
+    const int REF_AD_YEAR = 1943;
+    const int REF_BS_MONTH = 1;
+    const int REF_AD_MONTH = 4;
+    const int REF_BS_DAY = 1;
+    const int REF_AD_DAY = 14;
+    
+
+
+    return 0;
 }
