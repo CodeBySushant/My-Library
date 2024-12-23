b26f1009f54c5c52e2547e940c9d780de75d4983
Sushant Sharma <itsofficialsushant@gmail.com>
2024-12-23 23:35:13 +0545

    Created a Function which will Convert BS To AD.

--- CODES/Date_ConversionBSAD.cpp
+++ CODES/Date_ConversionBSAD.cpp
@@ -9,7 +9,11 @@ int main(){
     const int REF_BS_DAY = 1;
     const int REF_AD_DAY = 14;
     
+    const int bs_month_days[] = {30, 32, 31, 32, 32, 30, 30, 30, 29, 30, 29, 30}; //Declared Days of Every Month in Year using Arrays
 
+    void bsToAd(int bs_year, int bs_month, int bs_day){
+        int ad_year = REF_AD_YEAR + (bs_year - REF_BS_YEAR);
+    }
 
     return 0;
 }
