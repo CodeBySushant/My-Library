d847c7a9b1b497395677a4a361297e18709708be
Sushant Sharma <itsofficialsushant@gmail.com>
2025-01-26 17:31:08 +0330

    Created server for 1st time

--- Node Js/server.js
+++ Node Js/server.js
@@ -1,1 +1,18 @@
 //Creating server using express
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+})
+
+app.get('/greet', (req, res) => {
+    res.send('Hi There How are You?');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+});
+
+//To check server in browser search: localhost:3000/greet
