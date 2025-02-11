1190c88a25e6d701267803be0ed2a7235859c487
Sushant Sharma <itsofficialsushant@gmail.com>
2025-02-11 10:21:27 -0800

    Leetcode Question To be Solved

---
+++ C++ DSA/Arrays/UniqueNoofOccurence.cpp
@@ -0,0 +1,12 @@
+/*Leetcode question 1206 unique no of occurence:
+    1 2 3 3 1 3
+    Here 1 is 2 times 2 is one time and 3 is 3 times so occurence is unique as 1 2 3 time its appearing
+    But if: 1 1 2 2 3
+    Here its not unique no of occurence because 1 and 2 are appearing for 2 times so...
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+}
