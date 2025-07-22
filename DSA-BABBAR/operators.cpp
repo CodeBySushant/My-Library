ff37f066a0b9631a43c213094a71e3e7351da38b
Sushant Sharma <itsofficialsushant@gmail.com>
2025-07-22 23:14:00 +0100

    Operators in c++

---
+++ DSA-BABBAR/operators.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //Arithmetic Operator (+, -, *, /)
+    int sum =  5 + 4;
+    cout<<sum<<endl;
+
+    int diff = 5 - 4;
+    cout<<diff<<endl;
+
+    int mul = 5 * 4;
+    cout<<mul<<endl;
+
+    int div = 5 / 4;
+    cout<<div<<endl;
+
+    //Relational Operator (==, >, <, >=, <=, !=)
+    //Logical Operator (&&, ||, !)
+    //Bitwise Operator (<<, >>, ^, ~)
+
+    return 0;
+}
