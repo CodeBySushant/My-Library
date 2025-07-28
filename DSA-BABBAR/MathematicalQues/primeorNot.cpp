34a3043932ac8fe507703225168b4a424cd40747
Sushant Sharma <itsofficialsushant@gmail.com>
2025-07-28 22:07:58 +0100

    Code to check a num is Prime or Not

--- DSA-BABBAR/MathematicalQues/primeorNot.cpp
+++ DSA-BABBAR/MathematicalQues/primeorNot.cpp
@@ -1,8 +1,29 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n;
-    cout<<"Enter any number: ";
-    cin>>n;
-    
+
+int main() {
+    int num;
+    cout << "Enter any number: ";
+    cin >> num;
+
+    if (num < 2) {
+        cout << "Not prime.";
+    } else {
+        bool isPrime = true;
+
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            cout << "Prime";
+        } else {
+            cout << "Not prime";
+        }
+    }
+
+    return 0;
 }
