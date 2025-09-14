6010e16a6b3b98f7e3ab27099862afb8fc0f4dcf
Sushant Sharma <itsofficialsushant@gmail.com>
2025-09-14 21:50:18 +0100

    Updated code for more feature

--- DSA-BABBAR/MathematicalQues/cgpaCalculator.cpp
+++ DSA-BABBAR/MathematicalQues/cgpaCalculator.cpp
@@ -9,7 +9,7 @@ struct Subject {
     int credit;
 };
 
-// Data for 8 semesters (can be customized)
+// Data for 8 semesters
 vector<vector<Subject>> semesters = {
     {{"Maths-I", 4}, {"Engineering Graphics", 4}, {"Computer Programming Lab", 2}, {"Physics Lab", 2}, {"PHE", 1}, {"Physics", 3}, {"Communication Skill", 3}, {"Computer Programming", 3}},       // Sem 1
     {{"Maths-II", 4}, {"Engineering Mechanics", 4}, {"Engineering Chemistry", 3}, {"Environmental Science", 3}, {"BEEE", 3}, {"Workshop Practice", 3}, {"BEEE Lab", 1}, {"Chemistry Lab", 1}, {"NSS", 1}}, // Sem 2
@@ -21,25 +21,17 @@ vector<vector<Subject>> semesters = {
     {{"Project II", 6}, {"Internship", 6}, {"Prof Elective II", 3}}                                   // Sem 8
 };
 
-int main() {
-    int sem;
-    cout << "Enter your current semester (1-8): ";
-    cin >> sem;
+// Function to calculate SGPA for a semester
+float calculateSGPA(int sem) {
+    vector<Subject> subjects = semesters[sem - 1];
+    vector<float> grades(subjects.size());
 
-    if (sem < 1 || sem > 8) {
-        cout << "Invalid semester entered. Exiting...\n";
-        return 1;
-    }
+    cout << "\nEnter your grades (on a scale of 0 to 10) for Semester " << sem << ":\n";
 
-    vector<Subject> currentSubjects = semesters[sem - 1];
-    vector<float> grades(currentSubjects.size());
-
-    cout << "\nEnter your grades (on a scale of 4 to 10) for the following subjects:\n";
-
-    for (size_t i = 0; i < currentSubjects.size(); ++i) {
+    for (size_t i = 0; i < subjects.size(); ++i) {
         float grade;
         do {
-            cout << currentSubjects[i].name << " (" << currentSubjects[i].credit << " credits): ";
+            cout << subjects[i].name << " (" << subjects[i].credit << " credits): ";
             cin >> grade;
 
             if (grade < 0 || grade > 10) {
@@ -50,13 +42,12 @@ int main() {
         grades[i] = grade;
     }
 
-    // Calculate SGPA
     float totalWeighted = 0;
     int totalCredits = 0;
 
-    for (size_t i = 0; i < currentSubjects.size(); ++i) {
-        totalWeighted += grades[i] * currentSubjects[i].credit;
-        totalCredits += currentSubjects[i].credit;
+    for (size_t i = 0; i < subjects.size(); ++i) {
+        totalWeighted += grades[i] * subjects[i].credit;
+        totalCredits += subjects[i].credit;
     }
 
     float sgpa = totalWeighted / totalCredits;
@@ -64,6 +55,81 @@ int main() {
     cout << "\n----------------------------------\n";
     cout << "Your SGPA for Semester " << sem << " is: " << sgpa << endl;
     cout << "----------------------------------\n";
+
+    return sgpa;
+}
+
+int main() {
+    int choice;
+
+    while (true) {
+        cout << "\n----- MENU -----\n";
+        cout << "1. Calculate SGPA for a single semester\n";
+        cout << "2. Calculate CGPA up to your current semester\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice (1-3): ";
+        cin >> choice;
+
+        if (choice == 1) {
+            int sem;
+            cout << "Enter the semester number (1-8): ";
+            cin >> sem;
+
+            if (sem < 1 || sem > 8) {
+                cout << "Invalid semester. Try again.\n";
+            } else {
+                calculateSGPA(sem);
+            }
+        } else if (choice == 2) {
+            int currentSem;
+            cout << "Enter your current semester (1-8): ";
+            cin >> currentSem;
+
+            if (currentSem < 1 || currentSem > 8) {
+                cout << "Invalid semester. Try again.\n";
+                continue;
+            }
+
+            float totalWeighted = 0;
+            int totalCredits = 0;
+
+            for (int sem = 1; sem <= currentSem; ++sem) {
+                cout << "\n--- Semester " << sem << " ---";
+                vector<Subject> subjects = semesters[sem - 1];
+                vector<float> grades(subjects.size());
+
+                for (size_t i = 0; i < subjects.size(); ++i) {
+                    float grade;
+                    do {
+                        cout << "\n" << subjects[i].name << " (" << subjects[i].credit << " credits): ";
+                        cin >> grade;
+
+                        if (grade < 0 || grade > 10) {
+                            cout << "Invalid grade! Enter a number between 0 and 10.\n";
+                        }
+                    } while (grade < 0 || grade > 10);
+
+                    grades[i] = grade;
+                }
+
+                for (size_t i = 0; i < subjects.size(); ++i) {
+                    totalWeighted += grades[i] * subjects[i].credit;
+                    totalCredits += subjects[i].credit;
+                }
+            }
+
+            float cgpa = totalWeighted / totalCredits;
+
+            cout << "\n----------------------------------\n";
+            cout << "Your CGPA up to Semester " << currentSem << " is: " << cgpa << endl;
+            cout << "----------------------------------\n";
+        } else if (choice == 3) {
+            cout << "Exiting program. Goodbye!\n";
+            break;
+        } else {
+            cout << "Invalid choice. Please enter 1, 2, or 3.\n";
+        }
+    }
 
     return 0;
 }
